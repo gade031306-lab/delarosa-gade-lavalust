5d4eddd447a85b0448672c4cfd239e1825d613a7
Gade Dela Rosa <gade.031306@gmail.com>
2026-09-04 16:40:30 +0800

    Configure Aiven database environment

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'   => 'mysql',
-    'hostname' => 'localhost',
-    'port'     => '3306',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'mydb',
+    'hostname' => getenv('DB_HOST') ?: '',
+    'port'     => getenv('DB_PORT') ?: '',
+    'username' => getenv('DB_USERNAME') ?: '',
+    'password' => getenv('DB_PASSWORD') ?: '',
+    'database' => getenv('DB_DATABASE') ?: '',
     'charset'  => 'utf8mb4',
     'dbprefix' => '',
 );
--- app/models/Usersmodel.php
+++ app/models/Usersmodel.php
@@ -1,12 +1,8 @@
 <?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class UsersModel extends Model
 {
     protected $table = 'users';
     protected $primary_key = 'id';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
 }
