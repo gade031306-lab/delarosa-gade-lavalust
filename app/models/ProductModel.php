a8381b8b1206326e5a22166fcbc211d778393be9
Gade Dela Rosa <gade.031306@gmail.com>
2026-09-09 18:39:57 +0800

    tite

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -1,10 +1,8 @@
 <?php
-
-namespace App\Models;
-
-use LavaLust\Database\Model;
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class ProductModel extends Model
 {
     protected $table = 'products';
+    protected $primary_key = 'id';
 }
